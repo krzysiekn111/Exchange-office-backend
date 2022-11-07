1405f58abae38e0f0e8c8c021759d66ba5f35fab
krzysiekn111 <krzysiekn111@gmail.com>
2022-11-07 10:50:07 +0100

    First commit

---
+++ src/main/java/pl/exchangeofficebackend/ExchangeOfficeBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.exchangeofficebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeOfficeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeOfficeBackendApplication.class, args);
+    }
+
+}
