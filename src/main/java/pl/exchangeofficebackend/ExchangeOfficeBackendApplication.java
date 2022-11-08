eb101b9d88d371f6213f572b7fc4569fbc2e2d1e
krzysiekn111 <krzysiekn111@gmail.com>
2022-11-08 11:06:00 +0100

    Dodanie swaggera

--- src/main/java/pl/exchangeofficebackend/ExchangeOfficeBackendApplication.java
+++ src/main/java/pl/exchangeofficebackend/ExchangeOfficeBackendApplication.java
@@ -2,8 +2,10 @@ package pl.exchangeofficebackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class ExchangeOfficeBackendApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/pl/exchangeofficebackend/configuration/Config.java
@@ -0,0 +1,13 @@
+package pl.exchangeofficebackend.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@EnableSwagger2
+@Configuration
+@EnableWebMvc
+public class Config {
+
+}
